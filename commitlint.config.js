2436c6aa72fc87a32bdae0fbee07cb37c965750c
snack <novles@naver.com>
2023-01-19 01:54:13 +0900

    config(common): 📚 commitlint 설정 변경

--- commitlint.config.js
+++ commitlint.config.js
@@ -19,57 +19,55 @@ module.exports = {
 			type: '커밋 유형을 선택해주세요.',
 			scope: '변경 영향 범위를 선택해주세요.',
 			subject: '제목을 짧고 명시적으로 입력해주세요.:\n',
-			body: '변경에 대한 자세한 설명을 제공하십시오(선택사항). 줄바꿈 = "|":\n',
-			breaking: 'List any BREAKING CHANGES (optional). Use "|" to break new line:\n',
-			footerPrefixsSelect: 'Select the ISSUES type of changeList by this change (optional):',
-			customFooterPrefixs: 'Input ISSUES prefix:',
-			footer: 'List any ISSUES by this change. E.g.: #31, #34:\n',
-			confirmCommit: 'Are you sure you want to proceed with the commit above?'
+			body: '(선택사항) 변경에 대한 자세한 설명을 제공하십시오. 줄바꿈 = "|":\n',
+			footerPrefixsSelect: '(선택사항) 연관 ISSUES 상태를 선택해주세요.',
+			footer: '연관 ISSUES 입력해주세요. E.g.: #31, #34:\n',
+			confirmCommit: '이대로 커밋 하시겠습니까?'
 		},
 		types: [
 			{
 				value: 'wip',
-				name: 'WIP:     🔨  작업중...',
+				name: '🔨  작업중...',
 				emoji: '🔨'
 			},
 			{
 				value: 'feat',
-				name: 'feat:     🚀  새로운 기능 추가',
+				name: '🚀  새로운 기능 추가',
 				emoji: '🚀'
 			},
 			{
 				value: 'test',
-				name: 'test:     ✅  테스트 코드',
+				name: '✅  테스트 코드',
 				emoji: '✅'
 			},
 			{
 				value: 'fix',
-				name: 'fix:      🧩  버그 수정',
+				name: '🧩  버그 수정',
 				emoji: '🧩'
 			},
 			{
 				value: 'revert',
-				name: 'revert:   ⏪️  이번 버전 되돌리기',
+				name: '⏪️  이번 버전 되돌리기',
 				emoji: '⏪️'
 			},
 			{
 				value: 'config',
-				name: 'config:     📚  프로젝트 설정',
+				name: '📚  프로젝트 설정',
 				emoji: '📚'
 			},
 			{
 				value: 'style',
-				name: 'style:    🎨  코드 포맷팅, 세미',
+				name: '🎨  코드 포맷팅, 세미',
 				emoji: '🎨'
 			},
 			{
 				value: 'refactor',
-				name: 'refactor: ♻️  기존 코드에 대한 리펙토링',
+				name: '♻️  기존 코드에 대한 리펙토링',
 				emoji: '♻️'
 			},
 			{
 				value: 'perf',
-				name: 'perf:     ⚡️  성능 이슈 대응',
+				name: '⚡️  성능 이슈 대응',
 				emoji: '⚡️'
 			}
 		],
@@ -91,14 +89,14 @@ module.exports = {
 		],
 		allowCustomScopes: false,
 		allowEmptyScopes: true,
-		emptyScopesAlias: '생략',
+		emptyScopesAlias: 'skip',
 		upperCaseSubject: false,
-		allowBreakingChanges: ['feat', 'fix'],
+		allowBreakingChanges: [],
 		breaklineNumber: 100,
 		breaklineChar: '|',
 		skipQuestions: [],
 		issuePrefixs: [{ value: 'closed', name: '완료:  issues 종료.' }],
-		allowCustomIssuePrefix: true,
+		allowCustomIssuePrefix: false,
 		allowEmptyIssuePrefix: true,
 		confirmColorize: true,
 		maxHeaderLength: Infinity,
